d30ee13d3b017c70734f33c15c5957ecbe76f2fe
aledeejk <aledee@sfedu.ru>
2025-04-24 23:43:02 +0300

    4/295

--- koans/about_asserts.rb
+++ koans/about_asserts.rb
@@ -19,18 +19,18 @@ class AboutAsserts < Neo::Koan
   # To understand reality, we must compare our expectations against
   # reality.
   def test_assert_equality
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert expected_value == actual_value
   end
 
   # Some ways of asserting equality are better than others.
-  def test_a_better_way_of_asserting_equality
-    expected_value = __
+  def test_assert_equality
+    expected_value = 2
     actual_value = 1 + 1
 
-    assert_equal expected_value, actual_value
+    assert expected_value == actual_value
   end
 
   # Sometimes we will ask you to fill in the values
