85cb4bb086570a551beb084d2149ed5751251946
aledeejk <aledee@sfedu.ru>
2025-04-25 17:11:16 +0300

    139/295

--- koans/about_regular_expressions.rb
+++ koans/about_regular_expressions.rb
@@ -160,6 +160,7 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal __, "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+    # Заменяет все совпадения (two -> t, three -> t)
+    assert_equal "one t-t", "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
   end
 end
