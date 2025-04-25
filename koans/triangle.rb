e1fe896766f55cbe85ca9399df7712c818d38642
aledeejk <aledee@sfedu.ru>
2025-04-25 17:55:31 +0300

    155-157/295

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -1,22 +1,29 @@
 # Triangle Project Code.
 
-# Triangle analyzes the lengths of the sides of a triangle
-# (represented by a, b and c) and returns the type of triangle.
-#
-# It returns:
-#   :equilateral  if all sides are equal
-#   :isosceles    if exactly 2 sides are equal
-#   :scalene      if no sides are equal
-#
-# The tests for this method can be found in
-#   about_triangle_project.rb
-# and
-#   about_triangle_project_2.rb
-#
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # Проверка на недопустимые значения сторон
+  if [a, b, c].any? { |side| side <= 0 }
+    raise TriangleError, "Стороны должны быть положительными"
+  end
+
+  # Сортируем стороны для проверки неравенства треугольника
+  sides = [a, b, c].sort
+
+  # Проверка неравенства треугольника
+  if sides[0] + sides[1] <= sides[2]
+    raise TriangleError, "Не выполняется неравенство треугольника"
+  end
+
+  # Определение типа треугольника
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
-# Error class used in part 2.  No need to change this code.
+# Error class used in part 2
 class TriangleError < StandardError
 end
