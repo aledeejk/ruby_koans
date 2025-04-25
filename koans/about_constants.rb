2d4bdce4305031cee37153c87fa5e9465896c893
aledeejk <aledee@sfedu.ru>
2025-04-25 16:54:43 +0300

    106/295

--- koans/about_constants.rb
+++ koans/about_constants.rb
@@ -1,13 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-C = "top level"
+C = "top level"  # Константа верхнего уровня
 
 class AboutConstants < Neo::Koan
 
-  C = "nested"
+  C = "nested"   # Константа внутри класса
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal "nested", C  # Поиск начинается с текущей области видимости
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
